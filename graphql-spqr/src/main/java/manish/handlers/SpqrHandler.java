5505f089e35497f1d574bdd7de29b6888692bb80
Manish Maheshwari <mmaheshwari@expedia.com>
2018-11-27 19:54:23 +0530

    Checkpoint - Hacking Introspection Query

--- graphql-spqr/src/main/java/manish/handlers/SpqrHandler.java
+++ graphql-spqr/src/main/java/manish/handlers/SpqrHandler.java
@@ -1,11 +1,16 @@
 package manish.handlers;
 
+import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
+import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by mmaheshwari on 22/11/18.
@@ -29,18 +34,75 @@ public class SpqrHandler {
         String query = routingContext.getBodyAsString();
         LOGGER.debug("request body: {}", query);
 
+        if(query.contains("IntrospectionQuery")){
+
+            LOGGER.debug(">>>> INTROSPECTION QUERY");
+
+            JsonObject json = routingContext.getBodyAsJson();
+            String introSpection = json.getString("query");
+            LOGGER.debug("Introspection element: {}", introSpection);
+            ExecutionResult executionResult = graphql.execute(introSpection);
+            LOGGER.debug("executionResult data: {}", executionResult.getData().toString());
+
+            Map<String, Object> mapData = executionResult.getData();
+           // String introspectionResult = executionResult.getData().toString();
+
+            Map<String, Object> result = new HashMap<>();
+            result.put("data", mapData); // you have to wrap it into a data json key!
+            LOGGER.debug("REsult data - {}" , result);
+            JsonObject jsonResult = new JsonObject(result);
+//            JsonObject jsonResult = new JsonObject(introspectionResult);
+            //JSONObject jsonResult = new JSONObject(result);
+            LOGGER.debug("Json REsult data - {}" , jsonResult.encodePrettily());
+            routingContext.response().end(jsonResult.toString());
+        } else if(query.contains("operationName")){
+            LOGGER.debug(">>>> Playground QUERY");
+
+            JsonObject json = routingContext.getBodyAsJson();
+            String introSpection = json.getString("query");
+            LOGGER.debug("Introspection element: {}", introSpection);
+            ExecutionResult executionResult = graphql.execute(introSpection);
+            LOGGER.debug("executionResult data: {}", executionResult.getData().toString());
+
+            Map<String, Object> mapData = executionResult.getData();
+            // String introspectionResult = executionResult.getData().toString();
+
+            Map<String, Object> result = new HashMap<>();
+            result.put("data", mapData); // you have to wrap it into a data json key!
+            LOGGER.debug("REsult data - {}" , result);
+            JsonObject jsonResult = new JsonObject(result);
+//            JsonObject jsonResult = new JsonObject(introspectionResult);
+            //JSONObject jsonResult = new JSONObject(result);
+            LOGGER.debug("Json REsult data - {}" , jsonResult.encodePrettily());
+            routingContext.response().end(jsonResult.toString());
+        }else{
+            LOGGER.debug(">>>> NORMAL QUERY");
+            ExecutionResult executionResult = graphql.execute(query);
+            LOGGER.debug("Execution result: {}", executionResult.getData().toString());
+            routingContext.response().end(executionResult.getData().toString());
+        }
+
+
+
+//        if (query.has("query") && query.getString("query").indexOf("IntrospectionQuery") != -1) {
+//            String introSpection = query.getString("query");
+//            Map<String, Object> result = new HashMap<>();
+//            result.put("data", graphQLService.query(introSpection)); // you have to wrap it into a data json key!
+//            return new JSONObject(result);
+//        }
+
+
 //    ExecutionResult executionResult = graphql.execute(ExecutionInput.newExecutionInput()
 //            .query(query)
-//            .operationName(routingContext.request().get("operationName"))
-//            .context(raw)
+//            .operationName(json.getString("operationName"))
+//            .context(routingContext.getBodyAsString())
 //            .build());
-//    return executionResult.toSpecification();
+    //executionResult.toSpecification();
 
 
-        ExecutionResult executionResult = graphql.execute(query);
+      //  ExecutionResult executionResult = graphql.execute(query);
 
-        LOGGER.debug("Execution result: {}", executionResult.getData().toString());
-        routingContext.response().end(executionResult.getData().toString());
+
     }
 
     public void handleFailure(RoutingContext routingContext) {
